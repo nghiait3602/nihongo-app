136ea1c3525f9d054a94c0dec1278dd17ab78567
Le Quoc Viet <vietanhien@gmail.com>
2024-03-05 14:35:42 +0700

    cap nhat bang chu cai hira va kata

---
+++ src/component/ChuCai.styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  nullContainer: {
+    width: 50,
+    height: 50,
+    marginHorizontal: 12,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  chuCaiContainer: {
+    width: 50,
+    height: 50,
+    marginHorizontal: 12,
+    marginVertical: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "gray",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  chuCai: {
+    fontSize: 24,
+    color: "black",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 10,
+  },
+  chuCaiFlatList: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
