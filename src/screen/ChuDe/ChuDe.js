289e68c3323d8c7aacca5a01af32d15ede9b409b
Nguyen Trung Nghia with Nan <ntrungnghia102@gmail.com>
2024-03-14 21:46:26 +0700

    Update

---
+++ src/screen/ChuDe/ChuDe.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from 'react';
+
+const ChuDe = () => {
+  return (
+    <View>
+      <Text>ChuDe</Text>
+    </View>
+  );
+};
+
+export default ChuDe;
+
+const styles = StyleSheet.create({});
