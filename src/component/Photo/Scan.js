e8f8d74a91305fb56f6aff99ff9418e1a57ebd83
Nguyen Trung Nghia with Nan <ntrungnghia102@gmail.com>
2024-03-27 23:34:44 +0700

    update

--- src/component/Photo/Scan.js
+++ src/component/Photo/Scan.js
@@ -1,12 +1,14 @@
-import { Alert, Image, View, StyleSheet, Text, Dimensions } from "react-native";
+import { Alert, Image, View, StyleSheet, Text, Dimensions } from 'react-native';
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
-} from "expo-image-picker";
-import { useState } from "react";
-import OutLineButton from "../UI/Button/OutLineButton";
-import { Colors } from "../../constants/colors";
+} from 'expo-image-picker';
+import { useState } from 'react';
+import OutLineButton from '../UI/Button/OutLineButton';
+import { Colors } from '../../constants/colors';
+import * as FileSystem from 'expo-file-system';
+import axios from 'axios';
 
 function ImagePicker({ onTakeImage }) {
   const [pickedImage, setPickedImage] = useState();
@@ -21,8 +23,8 @@ function ImagePicker({ onTakeImage }) {
     }
     if (cameraPermissionInfomation.status === PermissionStatus.DENIED) {
       Alert.alert(
-        "Insufficient Permission!",
-        "Bạn cần cấp quyền cho máy ảnh và thư viện ảnh để sử dụng chức năng này."
+        'Insufficient Permission!',
+        'Bạn cần cấp quyền cho máy ảnh và thư viện ảnh để sử dụng chức năng này.'
       );
       return false;
     }
@@ -41,8 +43,6 @@ function ImagePicker({ onTakeImage }) {
       quality: 0.5,
     });
 
-    console.log("Hinh anh:", image);
-
     setPickedImage(image.assets[0].uri); // Fix lỗi từ cú pháp cũ setPickedImage(image.uri)
     // onTakeImage(image.assets[0].uri);
   }
@@ -54,15 +54,46 @@ function ImagePicker({ onTakeImage }) {
   }
 
   const setText = () => {
-    return pickedImage === undefined ? "" : `Đây là: ${pickedImage}`;
+    return pickedImage === undefined ? '' : `Đây là: ${pickedImage}`;
   };
-
+  const handlerImage = async () => {
+    try {
+      if (!pickedImage) {
+        Alert('Vui lòng chọn ảnh, hoặc chụp ảnh mới!');
+        return;
+      }
+      const key = 'AIzaSyBWEeLdE-L6yrPCfcU_13kLTWPzWGI_UMc';
+      const apiUrl = `https://vision.googleapis.com/v1/files:annotate?key=${key}`;
+      const base64Image = await FileSystem.readAsStringAsync(pickedImage, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      const requestData = {
+        requests: [
+          {
+            image: {
+              content: base64Image,
+            },
+            features: [{ type: 'LABEL_DETECTION', maxResults: 5 }],
+          },
+        ],
+      };
+      const res = await axios.post(apiUrl, requestData);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
       <OutLineButton icon="camera" onPress={takeImageHandler}>
         Chụp Ảnh
       </OutLineButton>
+      {pickedImage && (
+        <OutLineButton icon="camera" onPress={handlerImage}>
+          Tra cứu
+        </OutLineButton>
+      )}
       <Text style={styles.text}>{setText()}</Text>
     </View>
   );
@@ -70,19 +101,19 @@ function ImagePicker({ onTakeImage }) {
 
 export default ImagePicker;
 
-var height = Dimensions.get("window").height;
-var width = Dimensions.get("window").width;
+var height = Dimensions.get('window').height;
+var width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   imagePreview: {
     width: width,
     height: height / 2,
     marginVertical: 8,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: Colors.backgroundSliver,
     borderRadius: 30,
-    overflow: "hidden",
+    overflow: 'hidden',
   },
   image: {
     width: width,
@@ -91,16 +122,16 @@ const styles = StyleSheet.create({
   text: {
     paddingTop: 10,
     fontSize: 10, //khi co du lieu that thay doi thanh 20
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     color: Colors.title,
     lineHeight: 18,
     paddingHorizontal: 10,
   },
   buttonContainer: {
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
+    justifyContent: 'flex-end',
+    alignItems: 'center',
     marginTop: 10,
   },
 });
